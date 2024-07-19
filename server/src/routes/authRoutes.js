3d4c3537aca83146fec2ca25b96b5d550b121cbc
AryanChopra <aryanchopra989@gmail.com>
2024-07-20 03:08:21 +0530

    code cleanup

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -4,6 +4,6 @@ const authController = require("../controllers/authController");
 
 router.post("/login", authController.login);
 router.post("/register", authController.register);
-router.post("/logout", authController.logout); // New logout route
+router.post("/logout", authController.logout); 
 
 module.exports = router;
